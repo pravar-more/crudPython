8ed2455ad4eb9fcb958ab23fbf1d63b32e80bd9b
Pravar More <88570821+pravar-more@users.noreply.github.com>
2025-01-06 17:14:18 +0530

    Create urls.py

---
+++ django_sql_crud_project/app_name/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('<int:pk>/', views.detail, name='detail'),
+    path('create/', views.create, name='create'),
+    path('<int:pk>/update/', views.update, name='update'),
+    path('<int:pk>/delete/', views.delete, name='delete'),
+]
